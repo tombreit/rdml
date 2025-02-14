8045ded44092a8cd57466ea3336f08ab60a2b538
Thomas Breitner <t.breitner@csl.mpg.de>
2025-02-14 16:12:54 +0100

    Removed duplicate AUTH_LDAP_GROUP_TYPE setting

--- rdml/settings/ldap.py
+++ rdml/settings/ldap.py
@@ -29,6 +29,12 @@ AUTH_LDAP_SERVER_URI = env.str("AUTH_LDAP_SERVER_URI")
 AUTH_LDAP_BIND_DN = env.str("AUTH_LDAP_BIND_DN")
 AUTH_LDAP_BIND_PASSWORD = env.str("AUTH_LDAP_BIND_PASSWORD")
 
+AUTH_LDAP_GLOBAL_OPTIONS = {
+    # ldap.OPT_PROTOCOL_VERSION: 3,
+    # ldap.OPT_REFERRALS: 0,
+    # ldap.OPT_X_TLS_CACERTFILE: '/path/to/pemfile',
+}
+
 # First check LDAPBackend, than ModelBackend
 AUTHENTICATION_BACKENDS = [
     "django_auth_ldap.backend.LDAPBackend",
@@ -36,20 +42,11 @@ AUTHENTICATION_BACKENDS = [
 ]
 AUTH_LDAP_GROUP_TYPE = ActiveDirectoryGroupType()
 AUTH_LDAP_FIND_GROUP_PERMS = True
-
+AUTH_LDAP_FIND_GROUP_PERMS = True
+AUTH_LDAP_CACHE_TIMEOUT = 60 * 60
 # https://django-auth-ldap.readthedocs.io/en/latest/reference.html#auth-ldap-cache-timeout
 AUTH_LDAP_CACHE_TIMEOUT = 0
-
-AUTH_LDAP_GLOBAL_OPTIONS = {
-    # ldap.OPT_PROTOCOL_VERSION: 3,
-    # ldap.OPT_REFERRALS: 0,
-    # ldap.OPT_X_TLS_CACERTFILE: '/path/to/pemfile',
-}
-
-if env.bool("DEBUG"):
-    ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-    ldap.set_option(ldap.OPT_DEBUG_LEVEL, 255)
-
+AUTH_LDAP_USER_ATTR_MAP = {"first_name": "givenName", "last_name": "sn", "email": "mail"}
 
 AUTH_LDAP_USER_SEARCH = LDAPSearchUnion(
     LDAPSearch(
@@ -63,12 +60,6 @@ AUTH_LDAP_GROUP_SEARCH = LDAPSearchUnion(
     LDAPSearch(f"{env.str('AUTH_LDAP_USERS_DN')}", ldap.SCOPE_SUBTREE, "(objectClass=group)"),
 )
 
-AUTH_LDAP_GROUP_TYPE = ActiveDirectoryGroupType()
-AUTH_LDAP_FIND_GROUP_PERMS = True
-AUTH_LDAP_CACHE_TIMEOUT = 60 * 60
-
-AUTH_LDAP_USER_ATTR_MAP = {"first_name": "givenName", "last_name": "sn", "email": "mail"}
-
 # https://django-auth-ldap.readthedocs.io/en/latest/groups.html#limiting-access
 AUTH_LDAP_MIRROR_GROUPS = env.list("AUTH_LDAP_MIRROR_GROUPS_LIST")
 
@@ -81,3 +72,7 @@ AUTH_LDAP_USER_FLAGS_BY_GROUP = {
     ),
     "is_superuser": (LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_SUPERUSERS"))),
 }
+
+if env.bool("DEBUG"):
+    ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+    ldap.set_option(ldap.OPT_DEBUG_LEVEL, 255)
