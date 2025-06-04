ec915ae78066929d8b0e736414372be1a65df98a
Thomas Breitner <t.breitner@csl.mpg.de>
2025-06-04 11:51:17 +0200

    Use abbreviation in django admin titles

--- rdml/admin.py
+++ rdml/admin.py
@@ -15,8 +15,8 @@ class RDAdminSite(admin.AdminSite):
         if branding:
             context.update(
                 {
-                    "site_header": f"{branding.organization_name} Research Data Management",
-                    "site_title": f"{branding.organization_abbr} Research Data Management Portal",
+                    "site_header": f"{branding.organization_abbr} Research Data Management",
+                    "site_title": f"{branding.organization_abbr} Research Data Management",
                     "index_title": f"Welcome to the {branding.organization_name} Research Data Management Portal",
                 }
             )
