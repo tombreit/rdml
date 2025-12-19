d37fbdd22804b469100f5a0bf460789ecf332c86
Thomas Breitner <t.breitner@csl.mpg.de>
2025-12-19 16:43:13 +0100

    Bumped nodejs packages, silence dart-sass if-function deprecation warnings

---
+++ .sassrc.js
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Thomas Breitner
+//
+// SPDX-License-Identifier: CC0-1.0
+
+module.exports = {
+  silenceDeprecations: ['import', 'mixed-decls', 'color-functions', 'global-builtin', 'if-function'], 
+}
